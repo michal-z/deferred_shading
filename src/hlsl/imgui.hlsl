8e3ba07e5d71fa4b1c9f596303541707a4acf094
michal-z <michalziulek@gmail.com>
2016-05-29 22:59:16 +0200

    Added DirectXMath. Updated EASTL.

--- src/hlsl/imgui.hlsl
+++ src/hlsl/imgui.hlsl
@@ -1,11 +1,11 @@
-struct VSInput
+struct VsInput
 {
     float2 Position : POSITION;
     float2 Texcoord : TEXCOORD;
     float4 Color : COLOR;
 };
 
-struct VSOutput
+struct VsOutput
 {
     float4 Position : SV_Position;
     float2 Texcoord : TEXCOORD;
@@ -18,9 +18,9 @@ struct STransform
 };
 ConstantBuffer<STransform> gTransform : register(b0);
 
-VSOutput VSImgui(VSInput i)
+VsOutput VsImgui(VsInput i)
 {
-    VSOutput o;
+    VsOutput o;
     o.Position = mul(gTransform.Mat, float4(i.Position, 0.0f, 1.0f));
     o.Texcoord = i.Texcoord;
     o.Color = i.Color;
@@ -31,7 +31,7 @@ VSOutput VSImgui(VSInput i)
 Texture2D gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
-float4 PSImgui(VSOutput i) : SV_Target0
+float4 PsImgui(VsOutput i) : SV_Target0
 {
     return i.Color * gTexture.Sample(gSampler, i.Texcoord);
 }
