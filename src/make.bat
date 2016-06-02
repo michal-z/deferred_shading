0761cd1a40e5ce1751d6f1fad429e251689f2a60
michal-z <michalziulek@gmail.com>
2016-06-02 13:16:40 +0200

    Added executable file.

--- src/make.bat
+++ src/make.bat
@@ -3,5 +3,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
 cd ..\vc2015
-msbuild /nologo /verbosity:quiet /p:Configuration=Debug deferred_shading.sln
+msbuild /nologo /verbosity:quiet /p:Configuration=Release deferred_shading.sln
 cd ..\src
